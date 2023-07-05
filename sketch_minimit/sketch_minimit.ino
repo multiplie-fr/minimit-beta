138ab3acb61a5e21dc9bb8eea1225a054b83a16d
choueche <cecile.adam@gmail.com>
2023-07-05 14:33:10 +0200

    gestion touche CONN/FIN

--- sketch_minimit/sketch_minimit.ino
+++ sketch_minimit/sketch_minimit.ino
@@ -45,7 +45,7 @@ String currentService;  //le minimit service courant, voir si ca sert encore
 void setup() {
   Serial.begin(115200);
   Serial.println("**** Minimit is launching");
-  minitel.aiguillage(true, CODE_EMISSION_CLAVIER, CODE_RECEPTION_CLAVIER);
+  //minitel.aiguillage(true, CODE_EMISSION_CLAVIER, CODE_RECEPTION_CLAVIER);
   minitel.pageMode();
   minitel.noCursor();
   minitel.echo(false);
@@ -273,16 +273,19 @@ int launchService(String minimit_service) {
 // j'ajoute init parce que l'idée est dêtre sur que les paramètres sont bien rétablis
 // au cas où des services ont fait n'importe quoi
 void init_and_displayMire(boolean deconnecter) {
-
+  delay(1000);
   currentService = "";
   //minitel.connexion(false); // Si je mets ca la, ca s'arrete quand on vient de demarrage et qu'on est pas connecté
-  if (deconnecter) {
+  if (deconnecter && lasttouche==4953) {
     minitel.connexion(false);
+  }
+  else
+  {
+    //minitel.connexion(true);
   }
   saisieColor = CARACTERE_JAUNE;
   minitel.attributs(CARACTERE_JAUNE);
-
-  minitel.aiguillage(true, CODE_EMISSION_CLAVIER, CODE_RECEPTION_CLAVIER);
+  //minitel.aiguillage(true, CODE_EMISSION_CLAVIER, CODE_RECEPTION_CLAVIER);
   minitel.pageMode();
   minitel.newScreen();
   minitel.newXY(1, 0);
@@ -290,11 +293,8 @@ void init_and_displayMire(boolean deconnecter) {
   minitel.print(" ");
   minitel.repeat(30);
   minitel.attributs(CARACTERE_BLANC);
-
   String mire = "14,0c,1f,41,41,0e,1b,5a,40,26,23,23,4d,30,1f,42,41,0e,1b,5a,31,09,38,12,43,1f,43,41,0e,1b,5a,40,46,12,42,26,40,1f,44,41,0e,1b,5a,41,31,21,21,09,38,1f,45,42,0e,1b,5a,2b,2c,2c,26,1f,41,4a,0e,1b,54,20,12,5e,1f,42,4a,0e,1b,54,20,12,5e,1f,43,4a,0e,1b,54,20,12,5e,1f,44,4a,0e,1b,54,20,12,5e,1f,42,4b,0e,1b,54,20,12,42,30,20,20,30,20,12,42,30,30,1f,43,4b,0e,1b,57,1b,44,48,48,1b,54,1b,47,34,34,1b,57,1b,44,48,1b,54,1b,47,34,34,1b,57,1b,44,48,48,1b,54,1b,47,34,34,1b,57,1b,44,48,1f,44,4b,0e,1b,54,25,12,4a,29,1f,44,60,1b,4f,33,36,31,35,1f,45,4a,1b,44,60,12,5e,1f,46,4a,1b,44,1b,5d,18,53,69,20,6f,6e,20,19,42,65,74,61,69,74,20,65,6e,63,6f,72,65,20,64,61,6e,73,20,6c,65,73,20,1f,47,4a,1b,44,1b,5d,18,61,6e,6e,19,42,65,65,73,20,38,30,2c,20,6c,61,20,63,6f,6e,6e,65,78,69,6f,6e,20,64,65,1f,48,49,1b,5a,20,1b,5d,1b,44,18,63,65,20,6d,69,6e,69,74,65,6c,20,76,6f,75,73,20,61,75,72,61,69,74,20,63,6f,19,43,75,74,19,42,65,1f,49,49,1b,5a,20,1b,44,1b,5d,18,30,2c,31,32,46,20,19,41,61,20,6c,61,20,63,6f,6e,6e,65,78,69,6f,6e,1f,4a,49,1b,5a,20,1b,44,1b,5d,18,70,75,69,73,20,31,2c,32,39,46,20,6c,61,20,6d,69,6e,75,74,65,1f,4b,49,1b,5a,20,1b,44,1b,5d,18,64,6f,6e,74,20,46,2e,54,45,4c,45,43,4f,4d,20,30,2c,31,32,20,19,41,61,20,30,2c,35,30,46,2f,6d,69,6e,1f,4c,49,1b,5a,20,1b,5d,1b,45,18,20,12,45,53,45,52,56,49,43,45,53,20,44,49,53,50,4f,4e,49,42,4c,45,53,1f,4d,4a,1b,44,1b,5d,18,41,6e,6e,75,61,69,72,65,2c,20,6d,19,42,65,74,19,42,65,6f,2c,20,61,63,74,75,61,6c,69,74,19,42,65,73,2c,1f,4e,4a,1b,44,1b,5d,18,61,73,74,72,6f,2c,20,74,61,72,6f,74,73,2e,12,42,20,12,42,74,61,70,65,7a,20,47,55,49,44,45,1f,4f,41,1b,44,60,12,67,1f,50,41,63,6f,64,65,0d,0a,64,75,20,73,65,72,76,69,63,65,3a,1b,43,2e,12,5c,1b,44,0a,60,12,67,0a,28,63,29,0a,0d,6d,75,6c,74,69,70,6c,69,19,42,65,0a,0d,32,30,32,33,1f,53,61,1b,5d,20,45,6e,76,6f,69,20,20,1f,56,4b,1b,4d,41,4e,4e,55,41,49,52,45,20,44,45,53,20,53,45,52,56,49,43,45,53,1b,5a,20,1b,5d,20,47,75,69,64,65,20,20,1f,57,4b,50,61,72,61,6d,19,41,65,74,72,65,73,20,64,75,20,6d,69,6e,69,6d,69,74,1b,5a,20,1b,5d,1b,42,53,6f,6d,6d,61,69,72,65,1f,58,51,51,75,69,20,61,20,66,61,69,74,20,19,4b,63,61,20,3f,20,1b,42,1b,5d,20,53,75,69,74,65,20,20,1f,51,4c";
   checkScreen(mire, 0, 0);
   champVide(12, 17, 29);
   minitel.echo(true);
-
-  
-}
+ }
